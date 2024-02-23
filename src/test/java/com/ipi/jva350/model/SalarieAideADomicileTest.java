11dba5bed1b9c3352d530ce17c74615a578c75c4
salimaAIT <126787428+salimaAIT@users.noreply.github.com>
2024-02-23 13:37:27 +0100

    Tests paramétrés

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -1,9 +1,18 @@
 package com.ipi.jva350.model;
 
+import com.ipi.jva350.exception.SalarieException;
+import com.ipi.jva350.repository.SalarieAideADomicileRepository;
+import com.ipi.jva350.service.SalarieAideADomicileService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
@@ -98,5 +107,34 @@ public class SalarieAideADomicileTest {
               LocalDate.parse(dateFin));
       // Then : Comparaison du résultat de la méthode ou de l'état final avec celui attendu
       Assertions.assertEquals(expectedNb,resNb.size());
-                        }
+     }
+
+
+    @ExtendWith(MockitoExtension.class)
+    public class SalarieAideADomicileServiceMockTest {
+
+        @Mock
+        private SalarieAideADomicileRepository salarieAideADomicileRepository;
+        @InjectMocks
+        private SalarieAideADomicileService salarieService = new SalarieAideADomicileService();
+
+        // mocké :
+        @Test
+        public void testAjouteConge() throws SalarieException {
+            // Given :
+            SalarieAideADomicile monSalarie = new SalarieAideADomicile("Paul",
+                    LocalDate.of(2022, 6, 28), LocalDate.of(2023, 11, 1),
+                    9, 2.5,
+                    80, 20, 8);
+            // When :
+            salarieService.ajouteConge(monSalarie, LocalDate.of(2024, 12, 17),
+                    LocalDate.of(2024, 12, 18));
+            // Then :
+            ArgumentCaptor<SalarieAideADomicile> salarieAideADomicileCaptor = ArgumentCaptor.forClass(SalarieAideADomicile.class);
+            Mockito.verify(salarieAideADomicileRepository, Mockito.times(1)).save(salarieAideADomicileCaptor.capture()); // arg capture !
+            Assertions.assertEquals(1L, salarieAideADomicileCaptor.getValue().getCongesPayesPrisAnneeNMoins1());
+        }
+
+    }
+
 }
